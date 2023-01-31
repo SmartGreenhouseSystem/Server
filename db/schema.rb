057f4794b02d03d9c240498eacc2a237f65016f4
zastrixarundell <zastrixarundell@gmail.com>
2023-01-31 23:26:59 +0100

    Added grouped measurements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_215315) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_222459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_215315) do
     t.datetime "recorded_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "unit"
+    t.string "measurement_type"
   end
 
 end
