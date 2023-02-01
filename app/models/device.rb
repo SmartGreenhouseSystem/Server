8a0cfee7347131957fa02773924fca268a745447
zastrixarundell <zastrixarundell@gmail.com>
2023-02-01 14:00:22 +0100

    Validates requirements for fields

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,4 +1,7 @@
 class Device < ApplicationRecord
+    validates :device_id, presence: true
+    validates :name, presence: true
+
     def active?
         updated_at > (DateTime.now - 5.seconds)
     end
