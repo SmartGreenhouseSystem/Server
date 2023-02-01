ad7e2e934ad694226e0a3ccf6568698fdb708993
zastrixarundell <zastrixarundell@gmail.com>
2023-02-01 02:01:33 +0100

    Added default look for pagination

--- app/controllers/data_centre_controller.rb
+++ app/controllers/data_centre_controller.rb
@@ -1,5 +1,16 @@
 class DataCentreController < ApplicationController
   def index
-    @measurements = Measurement.all
+    @measurements = Measurement.limit(page_params[:per_page]).offset(params[:page])
+    @page = page_params[:page] || 1
+    @count = Measurement.count
+    @pages = (@count / page_params[:per_page].to_f).ceil || 1
+    @start = ((@page.to_i - 1) * page_params[:per_page]) + 1
+    @end = @start + @measurements.count - 1
+  end
+
+  private
+
+  def page_params
+    params.permit(:page).merge(per_page: 15)
   end
 end
