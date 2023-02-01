14421146692eb66fb79d7e3cdb42e08fd4fa6d49
zastrixarundell <zastrixarundell@gmail.com>
2023-02-01 16:50:52 +0100

    Using non-null fields for devices

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_01_141327) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_01_155001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "devices", force: :cascade do |t|
-    t.string "device_id"
-    t.string "name"
+    t.string "device_id", null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
