d3887e3d1537038857b1c908481e08bb32505339
zastrixarundell <zastrixarundell@gmail.com>
2023-02-02 02:22:39 +0100

    Added logic to halt form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,21 @@ import "controllers"
 import "chartkick"
 import "Chart.bundle"
 import "./tabler.min"
+
+import jquery from "jquery"
+
+window.jquery = jquery
+window.$ = jquery
+
+import * as bootstrap from "bootstrap"
+
+window.bootstrap = bootstrap
+
+$('form').on('submit', function(e) {
+    console.log('wowza!');
+    e.preventDefault();
+    return
+    let modal = bootstrap.Modal.getInstance($(this).closest('.modal'))
+    modal.hide()
+    $('div.modal-backdrop:nth-child(2)').remove()
+});
